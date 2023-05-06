4f8b5617957c912eff66675a77059185a851fdc7
saksham15 <saksham.pathak@pocketfm.com>
2023-05-06 16:01:39 +0530

    config change

--- acbs/settings.py
+++ acbs/settings.py
@@ -148,11 +148,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_ROOT = 'home/acbsbackend/static'
-MEDIA_ROOT = 'home/acbsbackend/media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
