08f8f9b83b1c1a853ed0df675cf6202c618a216d
saksham15 <saksham.pathak@pocketfm.com>
2023-05-16 22:10:04 +0530

    new config

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Annoucement, Sponser, AboutUs, Executive, MemberCountries
+from .models import Annoucement, Sponser, AboutUs, Executive, MemberCountries, Download, Rule, PastChampion, Contact
 
 # Register your models here.
 admin.site.register(Annoucement)
@@ -8,3 +8,7 @@ admin.site.register(Sponser)
 admin.site.register(AboutUs)
 admin.site.register(Executive)
 admin.site.register(MemberCountries)
+admin.site.register(Download)
+admin.site.register(Rule)
+admin.site.register(Contact)
+admin.site.register(PastChampion)
